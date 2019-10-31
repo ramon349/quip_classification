cdf1faa4b24ce97347c8ab1ccfb12e4bc3394dc3
ramon <rlcorre@emory.edu>
2019-10-31 14:31:28 -0400

    fixed common settings

--- common_settings.sh
+++ common_settings.sh
@@ -8,6 +8,6 @@ GOOGLE_PROJECT_ID=cloudypipelines
 ## Notes: public: us.gcr.io   private: gcr.io
 public_docker_host=us.gcr.io
 image_name=til_segmentation
-image_tag=0.1
+image_tag=1.1
 container_name=til_segmentation
 dockerfile=Dockerfile
